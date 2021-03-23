41306dc3b255dfdff23ca7293ae23812af452e80
summerscar <shen.yus@163.com>
2021-03-23 17:23:58 +0800

    📃docs(init): project

---
+++ README.md
@@ -0,0 +1,9 @@
+# YouTube-Live-Danmu
+
+油猴脚本
+
+为 Youtube 直播增加弹幕支持
+
+![](/preview/00.png)
+
+![](/preview/01.png)
---
+++ src/index.js
@@ -0,0 +1,330 @@
+// ==UserScript==
+// @name         YouTube Live Danmu
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Youtube Live Danmu
+// @author       summerscar
+// @match        https://www.youtube.com/*
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        none
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+
+    let defaultSpeed = 10
+    let defaultMaxLines = 10
+    let defaultOpacity = 0.9
+    let defaultColor = 'white'
+    let defaultFontSize = 20
+    let defaultTextShadow = '0 0 5px black'
+
+    GM_config.init(
+        {
+            'id': 'DanmuConfig',
+            'title': '弹幕设置',
+            'fields':
+            {
+                'turnON':
+                {
+                    'label': '开启弹幕',
+                    'type': 'checkbox',
+                    'default': true
+                },
+                'fontSize':
+                {
+                    'label': '字号',
+                    'type': 'int',
+                    'default': defaultFontSize
+                },
+                'color':
+                {
+                    'label': '颜色',
+                    'type': 'text',
+                    'default': defaultColor
+                },
+                'opacity':
+                {
+                    'label': '透明度',
+                    'type': 'float',
+                    'default': defaultOpacity
+                },
+                'textShadow':
+                {
+                    'label': '弹幕阴影(textShadow css)',
+                    'type': 'text',
+                    'default': defaultTextShadow
+                },
+                'speed':
+                {
+                    'label': '速度（s）',
+                    'type': 'int',
+                    'default': defaultSpeed
+                },
+                'maxLines':
+                {
+                    'label': '行数',
+                    'type': 'int',
+                    'default': defaultMaxLines
+                }
+            },
+            'events':
+            {
+                'init': function () { },
+                'open': function () { },
+                'save': function () {
+                    let config = {
+                        danmuStyle: {
+                            fontSize: GM_config.get('fontSize'),
+                            color: GM_config.get('color'),
+                            opacity: GM_config.get('opacity'),
+                            textShadow: GM_config.get('textShadow')
+                        },
+                        turnON: GM_config.get('turnON'),
+                        speed: GM_config.get('speed'),
+                        maxLines: GM_config.get('maxLines')
+                    }
+                    setDanmuConfigTurnON(config.turnON)
+                    setDanmuConfig(config)
+                    localStorage.setItem('danmuConfig', JSON.stringify(config))
+                    GM_config.close()
+                },
+                'close': function () { },
+                'reset': function () { }
+            }
+        });
+
+    // danmu Controller
+    class DanmuController {
+
+        constructor(config = {}) {
+            this.turnON = true
+            this.danmuStyle = {}
+            this.parent = config.parent
+            this.danmus = []
+            this.danmusEl = []
+            this.speed = config.speed || defaultSpeed
+            this.maxLines = config.maxLines || defaultMaxLines
+
+            this.danmuStyle.opacity = config.opacity || defaultOpacity
+            this.danmuStyle.color = config.color || defaultColor
+            this.danmuStyle.fontSize = config.fontSize || defaultFontSize
+            this.danmuStyle.textShadow = config.textShadow || defaultTextShadow
+        }
+        setDanmuTurnON(bol) {
+            this.turnON = bol
+        }
+        setDanmuConfig({ speed, maxLines, danmuStyle } = {}) {
+            speed ?? (this.speed = speed);
+            maxLines ?? (this.maxLines = maxLines);
+            this.danmuStyle = { ...this.danmuStyle, ...danmuStyle };
+        }
+        createDanmu(danmu) {
+            let danmuEl = document.createElement('span')
+            danmuEl.style.color = this.danmuStyle.color
+            danmuEl.style.opacity = this.danmuStyle.opacity
+            danmuEl.style.fontSize = this.danmuStyle.fontSize + 'px'
+            danmuEl.style.whiteSpace = 'nowrap'
+            danmuEl.style.textShadow = this.danmuStyle.textShadow
+            danmuEl.style.animation = `slidein ${this.speed}s linear`
+            danmuEl.style.top = `${this.danmus.length % this.maxLines * (this.danmuStyle.fontSize + 15)}px`
+            danmuEl.style.position = 'absolute'
+            danmuEl.style.display = 'inline-block'
+
+            danmuEl.innerHTML = danmu.message
+            danmuEl.setAttribute('data-timestamp', danmu.timestamp)
+            danmuEl.setAttribute('data-author', danmu.author)
+            danmuEl.onanimationend = () => {
+                this.removeDanmu(danmuEl)
+            };
+
+            this.danmusEl.push(danmuEl)
+            this.parent.appendChild(danmuEl)
+            // console.color(danmu.timestamp + ' ' + danmu.author + ': ' + danmu.message )
+            // this.clearTimeOutDanmu()
+        }
+        push(danmu) {
+            if (this.isPlaying === undefined) {
+                this.isPlaying = true
+            } else if (!this.isPlaying || !this.turnON) {
+                return
+            }
+            this.danmus.push(danmu)
+            this.createDanmu(danmu)
+        }
+        pause() {
+            this.isPlaying = false
+            this.danmusEl.forEach(function (item) { item.style.animationPlayState = 'paused' });
+        }
+        play() {
+            this.isPlaying = true
+            this.danmusEl.forEach(function (item) { item.style.animationPlayState = 'running' });
+        }
+        removeDanmu(danmuEl) {
+            danmuEl.remove()
+            this.danmusEl.splice(this.danmusEl.indexOf(danmuEl), 1)
+        }
+        clear() {
+            this.danmusEl.forEach(danmuEl => danmuEl.remove())
+            this.danmusEl = []
+        }
+    }
+    function formatTime(time) {
+        if (!time) return 0
+        time = time.replace('PM', '').replace('AM', '').trim()
+        const [hh, mm, ss] = time.padStart(8, '00:').split(':')
+        let timestamp = 0
+        timestamp += parseInt(hh) * 1000 * 60 * 60
+        timestamp += parseInt(mm) * 1000 * 60
+        timestamp += parseInt(ss) * 1000
+        return timestamp
+    }
+    console.color = function (string) {
+        let origin = console.log
+        origin.call(this, '%cYoutube Live Danmu: ' + string, 'color: red;font-size: 16px;')
+    }
+
+    //   聊天列表
+    const chatListElement = document.body.querySelector('#items.yt-live-chat-item-list-renderer')
+    if (!chatListElement) {
+        console.color('can not find chatListElement')
+        return
+    }
+
+    const playerContainer = window.top.document.body.querySelector('#player-container')
+    const videoRightControlBar = window.top.document.body.querySelector('.ytp-right-controls')
+
+    let danmuContainer
+    let danmuAnimateStyle
+    let danmuController
+    let videoEl
+    let observer
+    let videoIsSeeking = false
+    init()
+
+    function initAnimateCss() {
+        if (danmuAnimateStyle) {
+            danmuAnimateStyle.innerHTML = `@keyframes slidein {
+           from { transform: translateX(${playerContainer.clientWidth}px); }
+           to   { transform: translateX(-100%); }
+        }`
+            return
+        }
+        const head = window.top.document.querySelector('head')
+        let style = document.createElement('style')
+        style.innerHTML = `@keyframes slidein {
+         from { transform: translateX(${playerContainer.clientWidth}px); }
+         to   { transform: translateX(-100%); }
+       }`
+        danmuAnimateStyle = style
+        head.appendChild(style)
+    }
+    function init() {
+        if (!playerContainer) return
+        let div = document.createElement('div')
+        div.id = 'danmuContainer'
+        div.style.position = 'absolute'
+        div.style.left = '0'
+        div.style.top = '0'
+        div.style.width = '100%'
+        div.style.height = '100%'
+        div.style.overflow = 'hidden'
+        div.style.pointerEvents = 'none'
+        playerContainer.appendChild(div)
+        danmuContainer = div
+        initAnimateCss()
+        new ResizeObserver(initAnimateCss).observe(danmuContainer)
+
+        let danmuConfig = localStorage.getItem('danmuConfig')
+        let config = {}
+        if (danmuConfig) {
+            danmuConfig = JSON.parse(danmuConfig)
+            let { danmuStyle, ...restConfig } = danmuConfig
+            config = { ...danmuStyle, ...restConfig }
+            Object.entries(config).forEach(([key, val]) => {
+                GM_config.set(key, val);
+            })
+        }
+
+        danmuController = new DanmuController({ parent: danmuContainer, ...config })
+        addVideoEvent()
+        insertDanmuConfigBtn()
+        window.addEventListener('unload', function (event) {
+            observer && observer.disconnect()
+            setDanmuConfigTurnON(false)
+            console.color('unload')
+        });
+        console.color('inited !')
+    }
+    function insertDanmuConfigBtn() {
+        if (videoRightControlBar.querySelector('#DanmuConfigBtn')) return
+        const btn = document.createElement('button')
+        btn.id = 'DanmuConfigBtn'
+        btn.innerHTML = '弹幕'
+        btn.style.verticalAlign = 'top'
+        btn.classList.add('ytp-button')
+        btn.onclick = () => GM_config.open();
+        videoRightControlBar.insertBefore(btn, videoRightControlBar.firstChild)
+    }
+    function playDanmu() {
+        if (videoIsSeeking) return
+        danmuController.play()
+    }
+    function pauseDanmu() {
+        danmuController.pause()
+    }
+    function setIsSeeking() {
+        videoIsSeeking = true
+        pauseDanmu()
+    }
+    function setIsSeeked() {
+        videoIsSeeking = false
+        danmuController.play()
+    }
+    function addVideoEvent() {
+        videoEl = playerContainer.querySelector('video')
+        let isSeeking = false
+        videoEl.addEventListener('play', playDanmu)
+        videoEl.addEventListener('seeked', setIsSeeked)
+        videoEl.addEventListener('pause', pauseDanmu)
+        videoEl.addEventListener('seeking', setIsSeeking)
+    }
+    function setDanmuConfigTurnON(bol) {
+        danmuController.setDanmuTurnON(bol)
+        !bol && danmuController.clear()
+    }
+    function setDanmuConfig(config) {
+        danmuController.setDanmuConfig(config)
+    }
+    // observer callback
+    function observerCallback(mutationList) {
+        mutationList.forEach((mutation) => {
+            switch (mutation.type) {
+                case 'childList':
+                    if (!mutation.addedNodes.length) return
+
+                    [...mutation.addedNodes].forEach(function (el) {
+                        if (el.tagName.toLowerCase() === 'yt-live-chat-mode-change-message-renderer') return
+                        const message = el.querySelector('#content #message')?.textContent
+                        const timestamp = formatTime(el.querySelector('#content #timestamp')?.textContent)
+                        if (!timestamp) return
+                        const author = el.querySelector('#content yt-live-chat-author-chip')?.textContent
+                        danmuController.push({ timestamp, message, author })
+                    })
+                    break;
+                case 'attributes':
+                    break;
+
+            }
+        })
+    }
+    // observer
+    if (observer) observer.disconnect()
+    observer = new MutationObserver(observerCallback);
+    observer.observe(chatListElement, { subtree: false, childList: true });
+    console.color('start observer')
+
+})();
